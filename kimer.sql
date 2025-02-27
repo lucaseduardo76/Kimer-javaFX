fb027b9ea6d5904652631c8344f9d5637db5586a
Lucas Eduardo <lesds2001@gmail.com>
2025-02-27 17:10:24 -0300

    db

--- kimer.sql
+++ kimer.sql
@@ -7,6 +7,8 @@
 -- Versão do servidor: 10.4.32-MariaDB
 -- Versão do PHP: 8.0.30
 
+create database kimerdb;
+
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
